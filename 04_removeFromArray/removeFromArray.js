3a119862fffd5d67c06e9cd79035d27c0a0a28c1
Nando <ernandofebrian6@gmail.com>
2025-01-23 15:59:31 +0700

    Complete 04_removeFromArray task from Array and Loops section from The Odin Project lessons.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...removeElement) {
+    const removedArray = [];
+
+    for (let i = 0; i <= array.length - 1; i++) {
+        if (!removeElement.includes(array[i])) {
+            removedArray.push(array[i]);
+          }
+    }
+
+    return removedArray
 };
+
+console.log(removeFromArray([1, 2, 3, 4], 3));
+console.log(removeFromArray([1, 2, 3, 4], 3, 2));
 
 // Do not edit below this line
 module.exports = removeFromArray;

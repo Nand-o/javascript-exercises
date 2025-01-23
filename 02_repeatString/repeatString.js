00ed57af77bb8ddd7d0682afbd5437f4b7abfa00
Nando <ernandofebrian6@gmail.com>
2025-01-23 14:43:23 +0700

    Complete 02_repeatString task from Array and Loops section from The Odin Project lessons.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+    let answer = string;
 
+    if (num === 0) {
+        return answer = '';
+    } else if (num < 0) {
+        return answer = 'ERROR';
+    } else {
+        for (let i = 1; i <= (num - 1); i++) {
+            answer += string;
+        }
+        return answer
+    }
 };
 
 // Do not edit below this line

26816a46544ac9f9d529398da5dae9abb7cf0110
Nando <ernandofebrian6@gmail.com>
2025-01-23 16:13:58 +0700

    Complete 06_leapYears task from Array and Loops section from The Odin Project lessons.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function(years) {
+    if (years % 400 === 0) {
+        return true
+    } else if (years % 4 !== 0 || years % 100 === 0) {
+        return false
+    } else  {
+        return true
+    } 
 };
 
 // Do not edit below this line

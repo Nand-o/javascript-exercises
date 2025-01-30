2bbaf9fc61e7a3e4d03a9edc9e92496eb0aa8b9a
Nando <ernandofebrian6@gmail.com>
2025-01-30 13:03:32 +0700

    Complete 08_calculator task from Javascript -exercises task from The Odin Project lessons

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  return arr.reduce((total, number) => {
+    return total += number;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  return arr.reduce((total, number) => {
+    return total *= number;
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function (num, power) {
+  return num ** power;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  let arr = [];
+  for (let i = 1; i <= num; i++) {
+    arr.push(i);
+  }
+
+  return arr.reduce((total, number) => {
+    return total *= number;
+  }, 1);
 };
 
 // Do not edit below this line

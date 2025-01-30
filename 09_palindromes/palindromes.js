dd9e418b7ec82d0b5ee51eba490a43cad49a4e45
Nando <ernandofebrian6@gmail.com>
2025-01-30 13:45:02 +0700

    Complete 09_palindromes task from Javascript -exercises task from The Odin Project lessons

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const alpanume = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
+
+    const firstString = str
+        .toLowerCase()
+        .split('')
+        .filter((character) => alpanume.includes(character))
+        .join('');
+
+    const secondString = firstString.split('').reverse().join('');
+
+    return firstString === secondString;
 };
 
 // Do not edit below this line

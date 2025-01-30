51b2c85f887731da47fc3414d19164a13a86261e
Nando <ernandofebrian6@gmail.com>
2025-01-30 14:08:53 +0700

    Complete 10_fibonacci task from Javascript -exercises task from The Odin Project lessons

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+    const index = parseInt(num);
+    if (index < 0) return "OOPS";
+    let arrFibo = [0, 1];
+    for (let i = 2; i <= index; i++) {
+        let temp = arrFibo[i - 1] + arrFibo[i - 2];
+        arrFibo.push(temp);
+    }
+    return arrFibo[index]
 };
+
+console.log(fibonacci("10"));
 
 // Do not edit below this line
 module.exports = fibonacci;

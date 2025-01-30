1913539f644df1bbadd275efb453856a70172348
Nando <ernandofebrian6@gmail.com>
2025-01-30 14:47:23 +0700

    Complete 12_findTheOldest task from Javascript -exercises task from The Odin Project lessons

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function (arr) {
+    const oldest = arr
+        .sort((lastGuy, nextGuy) => getAge(lastGuy.yearOfBirth, lastGuy.yearOfDeath) > getAge(nextGuy.yearOfBirth, nextGuy.yearOfDeath)
+            ? -1 : 1);
 
+    return oldest[0]
+};
+
+function getAge (birth, death) {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
 };
 
 // Do not edit below this line
